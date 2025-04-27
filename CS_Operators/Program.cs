6e810909fa141af1f471b48bdefbdc502e2b2503
desiree <developwithdesiree@gmail.com>
2025-04-26 22:43:32 -0500

    Add project files.

---
+++ CS_Operators/Program.cs
@@ -0,0 +1,84 @@
+﻿// TODO: Practice Arithmetic Operators (+, -, *, /, %)
+int a = 15;
+int b = 4;
+
+// TODO 1: Add 'a' and 'b'
+Console.WriteLine($"Addition: {a + b}");
+
+// TODO 2: Subtract 'b' from 'a'
+Console.WriteLine($"Subtraction: {a - b}");
+
+// TODO 3: Multiply 'a' and 'b'
+Console.WriteLine($"Multiplication: {a * b}");
+
+// TODO 4: Divide 'a' by 'b'
+Console.WriteLine($"Division: {a / b}");
+
+// TODO 5: Find the remainder (modulus)
+Console.WriteLine($"Modulus: {a % b}");
+
+//****************************************************************
+// TODO: Practice Assignment Operators (=, +=, -=, *=, /=, %=)
+//****************************************************************
+int number = 10;
+Console.WriteLine($"Starting Number: {number}");
+
+// TODO 1: Add 5 using +=
+number += 5;
+Console.WriteLine($"After += 5: {number}");
+
+// TODO 2: Subtract 3 using -=
+number -= 3;
+Console.WriteLine($"After -= 3: {number}");
+
+// TODO 3: Multiply by 2 using *=
+number *= 2;
+Console.WriteLine($"After *= 2: {number}");
+
+// TODO 4: Divide by 4 using /=
+number /= 4;
+Console.WriteLine($"After /= 4: {number}");
+
+// TODO 5: Find modulus 3 using %=
+number %= 3;
+Console.WriteLine($"After %= 3: {number}");
+
+//****************************************************************
+// TODO: Practice Comparison Operators (==, !=, >, <, >=, <=)
+//****************************************************************
+
+int x = 10;
+int y = 20;
+
+// TODO 1: Check if x equals y
+Console.WriteLine($"x == y: {x == y}");
+
+// TODO 2: Check if x not equals y
+Console.WriteLine($"x != y: {x != y}");
+
+// TODO 3: Check if x greater than y
+Console.WriteLine($"x > y: {x > y}");
+
+// TODO 4: Check if x less than y
+Console.WriteLine($"x < y: {x < y}");
+
+// TODO 5: Check if x greater than or equal to y
+Console.WriteLine($"x >= y: {x >= y}");
+
+// TODO 6: Check if x less than or equal to y
+Console.WriteLine($"x <= y: {x <= y}");
+
+//******************************************************
+// TODO: Practice Logical Operators (&&, ||, !)
+//******************************************************
+bool isAdult = true;
+bool hasTicket = false;
+
+// TODO 1: Check if user is an adult AND has a ticket
+Console.WriteLine($"Adult AND Has Ticket: {isAdult && hasTicket}");
+
+// TODO 2: Check if user is an adult OR has a ticket
+Console.WriteLine($"Adult OR Has Ticket: {isAdult || hasTicket}");
+
+// TODO 3: Check NOT of isAdult
+Console.WriteLine($"NOT Adult: {!isAdult}");
